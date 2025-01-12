5c2e7d4abc827293f7ab6b4e3f1366e47b8b9a22
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-12 15:22:31 +0530

    home page UI updated

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,41 @@
+import { ScrollView, Pressable, Image } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 import ThemedView from "@/components/ThemedView";
 import ThemedText from "@/components/ThemedText";
 
+import { categories } from "@/constants/categories";
+
 const Home = () => {
   return (
     <ThemedView>
-      <ThemedText>Home</ThemedText>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={tw`gap-y-6 pb-3`}
+      >
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={tw`gap-x-3 px-2 mt-4`}
+        >
+          {categories.map((category) => {
+            return (
+              <Pressable
+                key={category.title}
+                style={tw`items-center gap-y-1.5`}
+              >
+                <Image
+                  source={category.image}
+                  style={tw`size-16 rounded-full`}
+                  resizeMode="stretch"
+                />
+                <ThemedText>{category.title}</ThemedText>
+              </Pressable>
+            );
+          })}
+        </ScrollView>
+      </ScrollView>
     </ThemedView>
   );
 };
---
+++ constants/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    title: "General",
+    image: require("../assets/images/general.png"),
+  },
+  {
+    title: "Technology",
+    image: require("../assets/images/technology.png"),
+  },
+  {
+    title: "Sports",
+    image: require("../assets/images/sports.png"),
+  },
+  {
+    title: "Science",
+    image: require("../assets/images/science.png"),
+  },
+  {
+    title: "Health",
+    image: require("../assets/images/health.png"),
+  },
+  {
+    title: "business",
+    image: require("../assets/images/business.png"),
+  },
+];
